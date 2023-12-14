c07aceb237d8b2758ce7b2929040b2547ec240a4
rostomyhsy <141927324+rostomyhsy@users.noreply.github.com>
2023-12-14 23:19:43 +0100

    get games

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -9,15 +9,21 @@ const controller = {}
 
 
 controller.viewGames = async (req, res,) => {
+
+
     try {
-        let games = await Games.find()
+
+        let games = await Games.find().populate("first_team").populate("second_team")
         res.status(200).json({
             "success": true,
             "games": games
         });
+
     } catch (error) {
+        console.log(error);
         return AppError.onError(error, "restaurant add error" + error);
     }
+    
 };
 
 
