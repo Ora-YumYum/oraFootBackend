98b28ef76e548beda6544007abe400d26018a8d9
rostomyhsy <141927324+rostomyhsy@users.noreply.github.com>
2023-12-14 05:53:57 +0100

    accepte player inv

--- src/controllers/player.js
+++ src/controllers/player.js
@@ -237,7 +237,7 @@ controller.accepteInvitation = async (req, res) => {
         },)
 
 
-        await Teams.updateOne({ _id: team_id, "players.player": new ObjectId(player_user_id) }, {
+        await Teams.updateOne({ _id: team_user_id, "players.player": new ObjectId(player_user_id) }, {
             "$set": {
                 "players.$.status": 0
             },
