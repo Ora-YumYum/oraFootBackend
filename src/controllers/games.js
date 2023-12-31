d1e0a86e441d5b92e139fc54bac3e4af5ae3d9ae
rostomyhsy <141927324+rostomyhsy@users.noreply.github.com>
2023-12-31 18:19:21 +0100

    get refree games

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -31,10 +31,8 @@ controller.activeGames = async (req, res,) => {
 
 
     games = await Games.find({
-        'refree': id,
         "games_status": 1,
 
-        
     }).populate({
         "path": "staduim",
         "select": "staduim_name wilaya user_id _id location cover_img"
